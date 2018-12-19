88c31c7532775fa2298faf499925e0b99d83748a
sureshkumar1211 <36293647+sureshkumar1211@users.noreply.github.com>
2018-12-20 00:06:58 +0530

    intoduction of js
    introduction of js and keywoprds

--- basics.js
+++ basics.js
@@ -1,1 +1,67 @@
 // basics of javascript
+// javascript-javascript is a lightweight and cross platform and object oriented programming language
+// javascript is a program that specifies behaviour of the webpage
+
+
+
+//some information about js
+//js was invented by Eich in 1995
+//ECMA-262 official name of standard 
+//ECMAscript is official name of js 
+
+//here are js basics topics
+//w3schools-basics-topics
+
+
+//javascript-statements
+//js statements are composed by operators,values,expressions,keywords and comments
+//js-keywords
+// break
+// continue
+// debugger
+// do
+// while
+// for
+// if
+// else
+// function
+// try
+// catch
+// var
+// let
+// const
+// return
+// switch
+//js keywords are reserved words. Reserved words are't used as names for variables
+
+
+
+
+
+
+
+
+
+
+// javascript-values
+//js has two varibles types :fixed values and variable values
+//varible values are called varibles
+var a,b,c;//How to declaring variables
+var a=10;//How to assign values to variables
+z=a+b;//How to compute values 
+//fixed values
+//fixed values are called literals
+10.10
+150
+
+
+//string
+//strings are text written both single and double quotes in js 
+'hello'
+"hello"
+ 
+
+
+
+
+
